b8995c6a75befbaa4b20b4a8355211c3dba88e82
whileNotEOThori <140858944+whileNotEOThori@users.noreply.github.com>
2024-11-02 18:33:33 +0200

    ITDCA2 assignment setup. Node class created

--- C#/ITDCA2/Formative Assessement/Assignment/Question 1/Node.cs
+++ C#/ITDCA2/Formative Assessement/Assignment/Question 1/Node.cs
@@ -1,58 +1,6 @@
-﻿
-namespace Question_1;
+﻿namespace Question_1;
 
-public class Node<T>
+public class Node
 {
-    private int index; //index variable for easy node access. Nodes are labelled with integersN
-    private T data; //data storage variable
-    private List<Node<T>> adjacencies; //adjacency list
-    private List<int> weights;
-
-    //default constructor
-    public Node()
-    {
-        adjacencies = new List<Node<T>>();
-        weights = new List<int>();
-    }
-
-    //value based constructor
-    public Node(T dataValue)
-    {
-        data = dataValue;
-        adjacencies = new List<Node<T>>();
-        weights = new List<int>();
-    }
-
-    public override string ToString()
-    {
-        // {Convert.ToString(this.data)} insert in data line
-        // this.
-        return $"Node index: {this.index}\nNode data: {Convert.ToString(this.N)}\nNumber of node adjacencies: {this.adjacencies.Count}";
-        // return Convert.ToString(data);
-    }
-
-    public int Index
-    {
-        get { return index; }
-        set { index = value; }
-    }
-
-    public T Data
-    {
-        get { return data; }
-        set { data = value; }
-    }
-
-    public List<Node<T>> Adjacencies
-    {
-        get { return adjacencies; }
-        set { adjacencies = value; }
-    }
-
-    public List<int> Weights
-    {
-        get { return weights; }
-        set { weights = value; }
-    }
 
 }
