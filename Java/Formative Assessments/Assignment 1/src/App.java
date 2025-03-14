c3894a4e521edf113b723101b2fd8b5bcd83995f
whileNotEOThori <140858944+whileNotEOThori@users.noreply.github.com>
2025-03-14 22:26:59 +0200

    Submission without logger

--- Java/Formative Assessments/Assignment 1/src/App.java
+++ Java/Formative Assessments/Assignment 1/src/App.java
@@ -7,94 +7,68 @@ import java.util.Scanner;
 public class App {
 
     public static Scanner scanner = new Scanner(System.in);
+    public static Inventory bookstore = new Inventory(); //CREATE BOOKSTORE
 
     public static void main(String[] args) throws Exception, BookNotFoundException {
-        int option = 0;
-        Inventory inventory = new Inventory();
-
-        do {
-            menu();
-            System.out.print("Enter option : ");
-            option = scanner.nextInt();
-
-            /*try {
-                
-            } catch (InputMismatchException e) {
-                e.getMessage();
-            }
-             */
-            switch (option) {
-                case 1:
-                    displayBooks(inventory.getInventory());
-                    break;
-                case 2:
-                    inventory.addBook(addBookToInventory());
-                    System.out.println("Book added to inventory");
-                    break;
-                case 3:
-                    System.out.print("Enter Title : ");
-                    String title = scanner.next();
-
-                    try {
-                        Book book = inventory.findBook(title);
-
-                        System.out.println("Book titled '" + title + "' found");
-                        System.out.println("Book [Title = " + book.getTitle() + ", Author = " + book.getAuthor() + ", Price = " + book.getPrice() + "]");
-
-                    } catch (BookNotFoundException e) {
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-                case 4:
-                    try {
-                        System.out.println("Create Order:");
-                        System.out.println("Customer details:");
-
-                        System.out.print("\tCustomer Name: ");
-                        String name = scanner.next();
-
-                        System.out.print("\tCustomer email: ");
-                        String email = scanner.next();
-
-                        Customer customer = new Customer(name, email);
-                        System.out.println("Processing Order for " + name);
-                        Order order = createOrder(inventory, customer);
-
-                        System.out.println("Order [Customer = Customer[Name = " + customer.getName() + ", Email: " + customer.getEmail() + "],");
-                        System.out.print("Books = [");
-                        for (int i = 0, numOrders = order.getBooks().size(); i < numOrders; i++) {
-                            System.out.print("Book [Title = " + order.getBooks().get(i).getTitle() + ", Author = " + order.getBooks().get(i).getAuthor() + ", Price = " + order.getBooks().get(i).getPrice() + "],");
-                        }
-                        System.out.println("]");
-
-                    } catch (InvalidOrderException e) {
-                        System.out.println(e.getMessage());
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-                case 99:
-                    System.out.println("Thank you for using the Bookstore System.");
-                    break;
-                default:
-                    System.out.println("Invalid option entered. Enter a number between 1 and 4");
-                    break;
-            }
-            System.out.println();
-        } while (option != 99);
-    }
-
-    private static void menu() {
         System.out.println("_______________________________");
         System.out.println("Welcome to the Bookstore System!");
         System.out.println("_______________________________\n");
 
-        System.out.println("Options:");
-        System.out.println("1.\tDisplay the available books");
-        System.out.println("2.\tAdd book to the inventory");
-        System.out.println("3.\tFind a book");
-        System.out.println("4.\tCreate an order");
-        System.out.println("99.\tExit");
+        //CREATE BOOKS
+        Book book1 = new Book("English HL", "Ntini", 75.00);
+        Book book2 = new Book("Afrikaans FAL", "Jonker", 75.00);
+        Book book3 = new Book("Mathematics", "Tinarow", 77.00);
+        Book book4 = new Book("Accounting", "Samuels", 77.00);
+        Book book5 = new Book("Life Orientation", "Joubert", 81.00);
+        Book book6 = new Book("Phsyical Sciences", "George", 82.00);
+        Book book7 = new Book("Information Technology", "Taute", 88.00);
+
+        //CREATE CUSTOMERS
+        Customer Thoriso = new Customer("Thoriso", "thoriso@gmail.com");
+        Customer Peter = new Customer("Peter", "peter@gmail.com");
+        Customer John = new Customer("John", "john@gmail");
+
+        // ADD BOOKS TO BOOK STORE
+        bookstore.addBook(book1);
+        bookstore.addBook(book2);
+        bookstore.addBook(book3);
+        bookstore.addBook(book4);
+        bookstore.addBook(book5);
+        bookstore.addBook(book6);
+
+        //CREATE ORDERS
+        Order order1 = new Order(Thoriso);
+        Order order2 = new Order(Peter);
+        Order order3 = new Order(John);
+
+        //DISPLAY ALL AVAILABLE BOOKS
+        displayBooks(bookstore.getInventory());
+
+        addBookToOrder(order1, book1);
+        addBookToOrder(order1, book2);
+        addBookToOrder(order1, book3);
+        addBookToOrder(order2, book4);
+        addBookToOrder(order2, book5);
+        addBookToOrder(order2, book6);
+        addBookToOrder(order2, book7); //ADD A BOOK THAT ISN'T IN THE BOOKSTORE INVENTORY
+
+        try {
+            processOrder(order1);
+        } catch (InvalidOrderException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            processOrder(order2);
+        } catch (InvalidOrderException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            processOrder(order3);
+        } catch (InvalidOrderException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private static void displayBooks(List<Book> inventory) {
@@ -106,51 +80,39 @@ public class App {
                 System.out.println("Book [Title = " + inventory.get(i).getTitle() + ", Author = " + inventory.get(i).getAuthor() + ", Price = " + inventory.get(i).getPrice() + "]");
             }
         }
+        System.out.println();
     }
 
-    private static Book addBookToInventory() {
-        System.out.println("Add book:");
-
-        System.out.print("Book Title: ");
-        String title = scanner.next();
-
-        System.out.print("Book Author: ");
-        String author = scanner.next();
-
-        System.out.print("Book Price: ");
-        double price = scanner.nextDouble();
-
-        return new Book(title, author, price);
+    private static void addBookToOrder(Order order, Book book) {
+        try {
+            order.addBook(bookstore.findBook(book.getTitle()));
+        } catch (BookNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private static Order createOrder(Inventory inventory, Customer customer) throws InvalidOrderException {
-        Order order = new Order(customer);
-
-        System.out.println("Enter the Title of the book you you would like to add to your order and -99 to stop.");
-        String title;
-        do {
-            System.out.print("Title:");
-            title = scanner.next();
-
-            try {
-                order.addBook(inventory.findBook(title));
-            } catch (BookNotFoundException e) {
-                System.out.println(e.getMessage());
-            }
-
-        } while (!title.equals("-99"));
+    private static void processOrder(Order order) throws InvalidOrderException {
+        System.out.println("Processing Order for " + order.getCustomer().getName());
 
         if (order.getBooks().size() > 0) {
-            return order;
+            System.out.print("Order [Customer = Customer[Name = " + order.getCustomer().getName() + ", Email: " + order.getCustomer().getEmail() + "],\nBooks = ");
+
+            int numOrders = order.getBooks().size();
+            for (int i = 0; i < numOrders - 1; i++) {
+                System.out.print("[Book [Title = " + order.getBooks().get(i).getTitle() + ", Author = " + order.getBooks().get(i).getAuthor() + ", Price = " + order.getBooks().get(i).getPrice() + "]],");
+            }
+            System.out.print("[Book [Title = " + order.getBooks().get(numOrders - 1).getTitle() + ", Author = " + order.getBooks().get(numOrders - 1).getAuthor() + ", Price = " + order.getBooks().get(numOrders - 1).getPrice() + "]]");
+            System.out.println("]");
+
+        } else {
+            throw new InvalidOrderException();
         }
-
-        throw new InvalidOrderException();
+        System.out.println();
     }
+
 }
-
-
 /*
-*create menu for displaying all books
+         create menu for displaying all books
  *c
 ode createOrder 
  */
